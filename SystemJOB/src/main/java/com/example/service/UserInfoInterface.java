aa0d55fa66322c0c1f4853aae684bdd868bb6a39
LiaoGuoXiong <73630146+LiaoGuoXiong@users.noreply.github.com>
2022-07-16 16:07:28 +0900

    Delete SystemJOB directory

--- SystemJOB/src/main/java/com/example/service/UserInfoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.bean.ClientBean;
-import com.example.bean.UserBean;
-
-public interface UserInfoInterface {
-    //用户注册
-    void register(UserBean user, ClientBean client);
-}
